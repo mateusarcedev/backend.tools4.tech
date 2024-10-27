aaadfbc77791d48e00508de9a60fafde5afeeb9a
Mateus Arce <arcemateuss@gmail.com>
2024-10-27 15:13:28 -0400

    descomentando swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
@@ -14,16 +14,16 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('DevLinks')
-  //   .setDescription(
-  //     'Este é um projeto criado para ajudar programadores iniciantes a encontrar ferramentas úteis para o desenvolvimento de softwares. Aqui você encontrará materiais para desenvolvimento frontend, backend e estudo de estruturas de dados. Sinta-se livre para contribuir.',
-  //   )
-  //   .setVersion('0.1')
-  //   .build();
+  const config = new DocumentBuilder()
+    .setTitle('DevLinks')
+    .setDescription(
+      'Este é um projeto criado para ajudar programadores iniciantes a encontrar ferramentas úteis para o desenvolvimento de softwares. Aqui você encontrará materiais para desenvolvimento frontend, backend e estudo de estruturas de dados. Sinta-se livre para contribuir.',
+    )
+    .setVersion('0.1')
+    .build();
 
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
