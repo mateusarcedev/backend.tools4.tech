12dfced46c26f06af5a3d8dac56b5107a9bec316
Mateus Arce <arcemateuss@gmail.com>
2024-12-28 09:56:47 -0400

    formating return categories

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -12,7 +12,11 @@ export class CategoriesService {
   }
 
   findAll() {
-    return this.prisma.category.findMany({})
+    return this.prisma.category.findMany({
+      select: {
+        name: true,
+      }
+    })
   }
 
   findOne(id: string) {
