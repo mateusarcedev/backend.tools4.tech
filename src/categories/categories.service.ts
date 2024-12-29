e328733f82daeaa813ce899aba1a457b40299206
Mateus Arce <arcemateuss@gmail.com>
2024-12-28 23:51:16 -0400

    add order by in categories

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -12,7 +12,11 @@ export class CategoriesService {
   }
 
   findAll() {
-    return this.prisma.category.findMany()
+    return this.prisma.category.findMany({
+      orderBy: {
+        name: 'asc'
+      }
+    })
   }
 
   findOne(id: string) {
